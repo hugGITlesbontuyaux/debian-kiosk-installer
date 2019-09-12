1b6d8971488114961a5a94a07ee4555bc275d7a7
hugGITlesbontuyaux <34125504+hugGITlesbontuyaux@users.noreply.github.com>
2019-09-12 12:05:46 +0200

    Update kiosk-installer.sh

--- kiosk-installer.sh
+++ kiosk-installer.sh
@@ -18,13 +18,13 @@ apt-get install \
 mkdir -p /home/kiosk/.config/openbox
 
 # create group
-groupadd kiosk
+# groupadd kiosk
 
 # create user if not exists
-id -u kiosk &>/dev/null || useradd -m kiosk -g kiosk -s /bin/bash 
+# id -u kiosk &>/dev/null || useradd -m kiosk -g kiosk -s /bin/bash 
 
 # rights
-chown -R kiosk:kiosk /home/kiosk
+# chown -R kiosk:kiosk /home/kiosk
 
 # remove virtual consoles
 if [ -e "/etc/X11/xorg.conf" ]; then
